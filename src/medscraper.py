a286a2313e1019aac247de9a40dbca3a2385c8b4
jgrainger-745 <jgrainger745@gmail.com>
2021-02-20 17:06:31 -0600

    creating a new branch for new tech use

--- src/medscraper.py
+++ src/medscraper.py
@@ -4,13 +4,8 @@ import requests
 
 
 
-med_path = 'https://api.endlessmedical.com/v1/dx/GetFeatures'        # Path for Ars Technica data
 
-def get_url(path):
-    raw = requests.get(path)  # This 'get' method is the one that has been imported from the requests library above
-    soup = raw.json() #BeautifulSoup(raw.text,
-            #             'html.parser')  # This 'BeautifulSoup' method is imported through from the bs4 library above
+med_path = 'https://api.endlessmedical.com/v1/dx/GetFeatures'
 
-    return soup
-
-print(get_url(med_path))
+df = pd.read_json(med_path)
+print(df)
