793410e8e666056d0e0b104113aaee8c91b16a12
jgrainger-745 <jgrainger745@gmail.com>
2021-03-06 15:06:53 -0600

    taking away a few unintended info

--- src/s3_load.py
+++ src/s3_load.py
@@ -4,8 +4,8 @@ import boto3
 s3 = boto3.resource(
     service_name ='s3',
     #region = 'us-east-2',
-    aws_access_key_id='AKIATA54LMTGBT4YV5NA',
-    aws_secret_access_key='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',
+    aws_access_key_id='',
+    aws_secret_access_key='',
 )
 
 
